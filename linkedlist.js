18d32dca59bdc219fdc8522c683ef090c1dbe2de
cooper <cooper.elliott30@gmail.com>
2023-04-04 17:53:59 +1000

    Update linked list class and tests

--- linkedlist.js
+++ linkedlist.js
@@ -1,8 +1,28 @@
 import Node from './node.js';
 
 export default class LinkedList {
+  // what if the node being passed to the linked list is a linked list?
+  // length would probably be annoying to calculate.
+  // so starting with an empty list every time is probably easiest and
+  // prone to less errors
+  /*
   constructor(head = null) {
     this.head = head;
+    if (this.head === null) {
+      this.length = 0;
+    } else {
+      this.length = 1;
+    }
+  }
+  */
+
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
+  head() {
+    return this.head;
   }
 
   append(value) {
--- linkedlist.test.js
+++ linkedlist.test.js
@@ -1,1 +1,27 @@
 import LinkedList from './linkedlist.js';
+import Node from './node.js';
+
+const list = new LinkedList();
+
+// const node = new Node();
+// const list2 = new LinkedList(node);
+
+describe('Linked List Tests', () => {
+  test('Properly creates list', () => {
+    expect(list);
+  });
+
+  test('Creating a new list with no arguments, new LinkedList(): list.head = null', () => {
+    expect(list.head).toBe(null);
+    expect(list.length).toBe(0);
+  });
+
+  //   test('Creating a new list from node, new LinkedList(node): list.head = node', () => {
+  //     expect(list2.head).toBe(node);
+  //     expect(list2.length).toBe(1);
+  //   });
+
+  //   test('Creating a new list from node, new LinkedList(node): list.head = { value: null, nextNode: null }', () => {
+  //     expect(list2.head).toEqual({ value: null, nextNode: null });
+  //   });
+});
