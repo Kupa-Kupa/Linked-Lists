01b98d05154abeeea72151cd895622edb03eb0fc
cooper <cooper.elliott30@gmail.com>
2023-04-05 14:49:52 +1000

    Add methods and tests for append and prepend

--- linkedlist.js
+++ linkedlist.js
@@ -21,31 +21,76 @@ export default class LinkedList {
     this.length = 0;
   }
 
-  head() {
+  // head returns the first node in the list
+  // if this is head() tests don't recognise it as a function?
+  // probably bc of methods and properties cant share a name?
+  headNode() {
     return this.head;
   }
 
+  // append(value) adds a new node containing value to the end of the list
   append(value) {
-    Node(value);
+    if (this.length === 0) {
+      this.head = new Node(value);
+      this.length++;
+      return;
+    }
+
+    let temp = this.head;
+
+    for (let i = 1; i <= this.length; i++) {
+      if (i === this.length) {
+        temp.nextNode = new Node(value);
+        this.length++;
+        return;
+      } else {
+        temp = temp.nextNode;
+      }
+    }
   }
 
-  prepend(value) {}
+  // prepend(value) adds a new node containing value to the start of the list
+  prepend(value) {
+    let temp = new Node(value);
+    temp.nextNode = this.head;
+    this.head = temp;
+    this.length++;
+    return;
+  }
 
-  size() {}
+  // size returns the total number of nodes in the list
+  size() {
+    return this.length;
+  }
 
+  // tail returns the last node in the list
   tail() {}
 
+  // at(index) returns the node at the given index
   at(index) {}
 
+  // pop removes the last element from the list
   pop() {}
 
-  contains(value) {}
+  // contains(value) returns true if the passed in value is in the list
+  // and otherwise returns false.
+  contains(value) {
+    if (LinkedList.length === 0) return false;
+  }
 
+  // find(value) returns the index of the node containing value, or null
+  // if not found.
   find(value) {}
 
+  // toString represents your LinkedList objects as strings, so you can
+  // print them out and preview them in the console. The format should
+  // be: ( value ) -> ( value ) -> ( value ) -> null
   toString() {}
 
+  // insertAt(value, index) that inserts a new node with the provided
+  // value at the given index.
   insertAt(value, index) {}
 
+  // removeAt(index) that removes the node at the given index.
   removeAt(index) {}
 }
--- linkedlist.test.js
+++ linkedlist.test.js
@@ -3,25 +3,50 @@ import Node from './node.js';
 
 const list = new LinkedList();
 
-// const node = new Node();
-// const list2 = new LinkedList(node);
+const list2 = new LinkedList();
+list2.append(1);
+
+const list3 = new LinkedList();
+list3.append(1);
+list3.append(2);
+
+const list4 = new LinkedList();
+list4.prepend(0);
+
+const list5 = new LinkedList();
+list5.append(1);
+list5.prepend(0);
 
 describe('Linked List Tests', () => {
-  test('Properly creates list', () => {
+  test('Properly creates list, new LinkedList(): list.head = null, list.length = 0', () => {
     expect(list);
-  });
-
-  test('Creating a new list with no arguments, new LinkedList(): list.head = null', () => {
     expect(list.head).toBe(null);
     expect(list.length).toBe(0);
   });
 
-  //   test('Creating a new list from node, new LinkedList(node): list.head = node', () => {
-  //     expect(list2.head).toBe(node);
-  //     expect(list2.length).toBe(1);
-  //   });
+  test('Test append(value) for an empty list: list.head = node, list.length = 1', () => {
+    expect(list2.head).toEqual({ value: 1, nextNode: null });
+    expect(list2.length).toBe(1);
+  });
 
-  //   test('Creating a new list from node, new LinkedList(node): list.head = { value: null, nextNode: null }', () => {
-  //     expect(list2.head).toEqual({ value: null, nextNode: null });
-  //   });
+  test('Test append(value) for an non-empty list: list.head = node, list.length = 2', () => {
+    expect(list3.head).toEqual({
+      value: 1,
+      nextNode: { value: 2, nextNode: null },
+    });
+    expect(list3.length).toBe(2);
+  });
+
+  test('Test prepend(value) for an empty list: list.head = node, list.length = 1', () => {
+    expect(list4.head).toEqual({ value: 0, nextNode: null });
+    expect(list4.length).toBe(1);
+  });
+
+  test('Test prepend(value) for an non-empty list: list.head = node, list.length = 3', () => {
+    expect(list5.head).toEqual({
+      value: 0,
+      nextNode: { value: 1, nextNode: null },
+    });
+    expect(list5.length).toBe(2);
+  });
 });
